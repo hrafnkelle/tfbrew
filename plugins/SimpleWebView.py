78ea7b2cd199a9e00b016103f07a0dc39ab36d37
Hrafnkell Eiriksson <he@klaki.net>
2018-03-12 23:30:59 +0000

    web can now be used to change state/send messages through PUT requests

--- plugins/SimpleWebView.py
+++ plugins/SimpleWebView.py
@@ -1,3 +1,4 @@
+from functools import partial
 import asyncio
 import struct
 import time
@@ -15,16 +16,26 @@ logger = logging.getLogger(__name__)
 
 def factory(name, settings):
     logger.info("Initializing SimpleWebView")
-    return SimpleWebView(name)
+    return SimpleWebView(name, settings['endpoints'])
 
 class SimpleWebView(interfaces.Component):
-    def __init__(self, name):
+    def __init__(self, name, endpoints):
         self.name = name
         self.endpointData = {}
         app.router.add_get('/', self.webView)
 
+        for name in endpoints:
+            app.router.add_put("/%s"%name, partial(self.handler, name))
+
     def callback(self, endpoint, data):
         self.endpointData[endpoint] = data
+
+    async def handler(self, name, request):
+        print("handling %s"%name)
+        stuff = await request.json()
+        print("endpoint %s got %s"%(name, stuff))
+        notify(Event(source=self.name, endpoint=name, data=stuff))
+        return web.json_response(stuff)
 
     def webView(self, request):
         return web.json_response(text=str(self.endpointData))
